43cbbc54d1edeb1b350d8ba1d1bb047bbba37dd5
MichaelVerdon <mwverdon2000@gmail.com>
2023-10-24 13:18:50 +0100

    setting up db connection

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_mysqldb import MySql
 
 app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "teammeng"
+app.config["MYSQL_DB"] = "questions"
 
 @app.route("/")
 def hello_world():
