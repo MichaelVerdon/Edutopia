68ecb2cbdc4b3d591983d9a511a49f95c17d9b32
MichaelVerdon <mwverdon2000@gmail.com>
2023-10-29 19:38:45 +0000

    fetch question by random and not asked already

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+import random
 
 from flask_mysqldb import MySQL
 
@@ -16,13 +17,28 @@ mysql = MySQL(app)
 def get_question():
     topic_id = request.args.get("topic_id")
 
+    # Check argument is valid
     if topic_id is None:
-        return jsonify({"error": "missing 'topic_id' paramter"}), 400
+        return jsonify({"error": "missing 'topic_id' parameter"}), 400
 
     cur = mysql.connection.cursor()
-    cur.execute(f"SELECT * FROM topic_questions WHERE topic_id = {topic_id}")
-    question = cur.fetchall()
+    cur.execute("SELECT * FROM topic_questions WHERE topic_id = %s", (topic_id,))
+    questions = cur.fetchall()
     cur.close()
+
+    # Return 404 if no questions found
+    if not questions:
+        return jsonify({"error": "No questions found for the specified topic_id"}), 404
+
+    session.setdefault("retrieved_questions", [])
+    unretrieved_questions = [
+        question
+        for question in questions
+        if question["id"] not in session["retrieved_questions"]
+    ]
+
+    # Select a random question from retrieved questions
+    question = unretrieved_questions[random.randint(0, len(questions) - 1)]
 
     return jsonify(question)
 
