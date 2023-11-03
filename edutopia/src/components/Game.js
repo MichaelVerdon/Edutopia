f28320b4d5314fee325313cd1e192e7303d1437c
UP2014806 <91476476+UP2014806@users.noreply.github.com>
2023-11-03 10:56:03 +0000

    splash page
    basic layout of the splash page

---
+++ edutopia/src/components/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="game">
+      <p>GAMEEEE</p>
+    </div>
+  );
+}
+
+export default Game;
---
+++ edutopia/src/components/LandingPage.js
@@ -0,0 +1,75 @@
+import './LandingPage.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function LandingPage() {
+    
+    const handlePlayClick = () => {
+        console.log('play game');
+    }
+
+    return (
+
+        <div className="landing-page">
+
+            <div class="hexagon-container">
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    E
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    d
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    u
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    t
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    o
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    p
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    i
+                    </div>
+                </div>
+
+                <div class="hexagon">
+                    <div class="hexagon-inner">
+                    a
+                    </div>
+                </div>
+            </div>
+            
+            <div className="button-container">
+                <Link to="/game">
+                    <button onClick={handlePlayClick} className="play-button">Play</button>
+                </Link>
+                <button className='options-button'>Options</button>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
