53b9bbb45e068d3e5f6a0b0b0777bd1edc12fe96
MichaelVerdon <mwverdon2000@gmail.com>
2023-12-20 08:05:08 +0000

    Dropped futile things

--- edutopia/src/pages/game/GameHandler.js
+++ edutopia/src/pages/game/GameHandler.js
@@ -5,12 +5,6 @@ class GameHandler{
         this.api_url = "http://localhost:9000/get_question?topic_id=";
     }
 
-    // To use with the API hooks in Game.js
-    get getQuery(){
-        var topic = topics[Math.floor(Math.random()*this.topics.length)];
-        return api_url + topic;
-    }
-
     // To check a player answered correctly or incorrectly 
     static questionStatus(question_data, answer){
         if(answer == question_data[6]){
