ace1b815e1653fec5b2f4162e61f74230c29d32e
Hana Franekova <72622217+UP930421@users.noreply.github.com>
2024-01-29 21:28:21 +0000

    all the elements needed added, fixed player object

--- edutopia/src/pages/game/PlayerObject.js
+++ edutopia/src/pages/game/PlayerObject.js
@@ -5,37 +5,37 @@ class PlayerObject{
     constructor(playerId) {
         this.playerId = playerId;
         this.liveStatus = true;
-        this._techPoints = 0; // Points
-        this._foodPoints = 0;
-        this._woodPoints = 0;
-        this._metalPoints = 0;
+        this.techPoints = 100; // Points
+        this.foodPoints = 100;
+        this.woodPoints = 100;
+        this.metalPoints = 100;
         // A list of tile objects
-        this._ownedTiles = [];
+        this.ownedTiles = [];
     }
 
     // Return Player ID For checks
-    get playerID(){return this.player_id;}
+    get getPlayerID(){return this.player_id;}
 
     // Get Methods for resource points
-    get techPoints(){return this.techPoints;}
-    get foodPoints(){return this.foodPoints;}
-    get woodPoints(){return this.woodPoints;}
-    get metalPoints(){return this.metalPoints;}
+    get getTechPoints(){return this.techPoints;}
+    get getFoodPoints(){return this.foodPoints;}
+    get getWoodPoints(){return this.woodPoints;}
+    get getMetalPoints(){return this.metalPoints;}
 
-    get ownedTiles(){return this.ownedTiles;}
+    get getOwnedTiles(){return this.ownedTiles;}
 
     // Checks for eliminating players
-    get liveStatus(){return this.alive}
+    get getLiveStatus(){return this.alive}
 
     // Set Methods for resource points
-    set techPoints(setPoints){this.techPoints = setPoints;}
-    set foodPoints(setPoints){this.foodPoints = setPoints;}
-    set woodPoints(setPoints){this.woodPoints = setPoints;}
-    set metalPoints(setPoints){this.metalPoints = setPoints;}
+    set setTechPoints(setPoints){this.techPoints = setPoints;}
+    set setFoodPoints(setPoints){this.foodPoints = setPoints;}
+    set setWoodPoints(setPoints){this.woodPoints = setPoints;}
+    set setMetalPoints(setPoints){this.metalPoints = setPoints;}
 
-    set ownedTiles(ownedTiles){this.ownedTiles = ownedTiles;}
+    set setOwnedTiles(ownedTiles){this.ownedTiles = ownedTiles;}
 
-    set liveStatus(status){this.alive = status;}
+    set setLiveStatus(status){this.alive = status;}
 
     // Methods for checking resources generated per turn
 
