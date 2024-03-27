d739de307be1dfc3b96469c255fb49950c863e94
MichaelVerdon <mwverdon2000@gmail.com>
2024-03-27 00:40:34 +0000

    Started Get Tile Logic

--- edutopia/src/pages/game/aiPlayer.js
+++ edutopia/src/pages/game/aiPlayer.js
@@ -9,7 +9,7 @@ class aiPlayer extends PlayerObject{
     }
 
     // Difficulty is set between 1 and 4 which will be used as a multiplier for
-    // probability or answering correctly. e.t.c difficulty of 2 means 2*0.25 = 0.5 probability of answering correctly
+    // probability or answering correctly. e.t.c difficulty of 2 means 2 * 0.25 = 0.5 probability of answering correctly
     setDifficulty(){
         return Math.floor(Math.random() * 4) + 1;
     }
@@ -17,14 +17,27 @@ class aiPlayer extends PlayerObject{
     answerQuestion(){
         const probability = this.difficulty * 0.25; // Probability of getting answer right
         const randomValue = Math.random(); // Generate a random number between 0 and 1
-
         // Check if the probability is greater which means the answer is correct
         if (randomValue <= probability) {
-            return true;
-        } else {
-            return false;
+            this.setTechPoints(this.getTechPoints + 5) // Add five tech points for a successful answer.
         }
     }
+
+    getClaimTile(){
+        // Will eventually be able to pick a random tile based off adjacent tiles using this.tiles
+        // Then apply logic to give resources
+        const ownedTileCount = this.ownedTiles.length
+        const randomTileIndex = Math.floor(Math.random() * ownedTileCount);
+        let randomTile = this.ownedTiles[randomTileIndex];
+        // Will need to bulletproof this later (e.t.c cases where a random tile has no adjacent ones)
+        return randomTile;
+    }
+
+    checkAdjacentTiles(tile){
+        let vacantTiles = [];
+    }
+
+
 }
 
 export default aiPlayer;
